a9c67941b0fc6782d08438f62a363ce90c376c7a
Gael Rial Costas <gael.rial.costas@gmail.com>
2020-06-28 11:13:48 +0200

    Added instructions bgezc ad blezc.

---
+++ src/main/java/net/jamsimulator/jams/mips/instruction/assembled/defaults/AssembledInstructionBgezc.java
@@ -0,0 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Gael Rial Costas
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.jamsimulator.jams.mips.instruction.assembled.defaults;
+
+import net.jamsimulator.jams.mips.instruction.Instruction;
+import net.jamsimulator.jams.mips.instruction.assembled.AssembledI16Instruction;
+import net.jamsimulator.jams.mips.instruction.basic.BasicInstruction;
+import net.jamsimulator.jams.mips.instruction.basic.defaults.InstructionBgezc;
+import net.jamsimulator.jams.utils.StringUtils;
+
+public class AssembledInstructionBgezc extends AssembledI16Instruction {
+
+	public AssembledInstructionBgezc(int targetRegister, int offset, Instruction origin, BasicInstruction<AssembledInstructionBgezc> basicOrigin) {
+		super(InstructionBgezc.OPERATION_CODE, targetRegister, targetRegister, offset, origin, basicOrigin);
+	}
+
+	public AssembledInstructionBgezc(int instructionCode, Instruction origin, BasicInstruction<AssembledInstructionBgezc> basicOrigin) {
+		super(instructionCode, origin, basicOrigin);
+	}
+
+	@Override
+	public String parametersToString(String registersStart) {
+		return registersStart + getTargetRegister()
+				+ ", 0x" + StringUtils.addZeros(Integer.toHexString(getImmediate()), 4);
+	}
+}
---
+++ src/main/java/net/jamsimulator/jams/mips/instruction/basic/defaults/InstructionBgezc.java
@@ -0,0 +1,92 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Gael Rial Costas
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.jamsimulator.jams.mips.instruction.basic.defaults;
+
+import net.jamsimulator.jams.mips.architecture.SingleCycleArchitecture;
+import net.jamsimulator.jams.mips.instruction.Instruction;
+import net.jamsimulator.jams.mips.instruction.assembled.AssembledI16Instruction;
+import net.jamsimulator.jams.mips.instruction.assembled.AssembledInstruction;
+import net.jamsimulator.jams.mips.instruction.assembled.defaults.AssembledInstructionBgezc;
+import net.jamsimulator.jams.mips.instruction.basic.BasicInstruction;
+import net.jamsimulator.jams.mips.instruction.execution.SingleCycleExecution;
+import net.jamsimulator.jams.mips.parameter.ParameterType;
+import net.jamsimulator.jams.mips.parameter.parse.ParameterParseResult;
+import net.jamsimulator.jams.mips.register.Register;
+import net.jamsimulator.jams.mips.register.Registers;
+import net.jamsimulator.jams.mips.simulation.Simulation;
+
+import java.util.Optional;
+
+public class InstructionBgezc extends BasicInstruction<AssembledInstructionBgezc> {
+
+	public static final String NAME = "Branch and link on greater than or equal to zero compact";
+	public static final String MNEMONIC = "bgezc";
+	public static final int OPERATION_CODE = 0b010110;
+
+	private static final ParameterType[] PARAMETER_TYPES = new ParameterType[]{ParameterType.REGISTER, ParameterType.SIGNED_16_BIT};
+
+	public InstructionBgezc() {
+		super(NAME, MNEMONIC, PARAMETER_TYPES, OPERATION_CODE);
+		addExecutionBuilder(SingleCycleArchitecture.INSTANCE, SingleCycle::new);
+	}
+
+	@Override
+	public AssembledInstruction assembleBasic(ParameterParseResult[] parameters, Instruction origin) {
+		return new AssembledInstructionBgezc(parameters[0].getRegister(), parameters[1].getImmediate(), origin, this);
+	}
+
+	@Override
+	public AssembledInstruction assembleFromCode(int instructionCode) {
+		return new AssembledInstructionBgezc(instructionCode, this, this);
+	}
+
+	@Override
+	public boolean match(int instructionCode) {
+		int rs = instructionCode >> AssembledI16Instruction.SOURCE_REGISTER_SHIFT & AssembledI16Instruction.SOURCE_REGISTER_SHIFT;
+		int rt = instructionCode >> AssembledI16Instruction.TARGET_REGISTER_SHIFT & AssembledI16Instruction.TARGET_REGISTER_MASK;
+		return super.match(instructionCode) && rs == rt && rs != 0;
+	}
+
+
+	public static class SingleCycle extends SingleCycleExecution<AssembledInstructionBgezc> {
+
+		public SingleCycle(Simulation<SingleCycleArchitecture> simulation, AssembledInstructionBgezc instruction) {
+			super(simulation, instruction);
+		}
+
+		@Override
+		public void execute() {
+			Registers set = simulation.getRegisters();
+			Optional<Register> rt = set.getRegister(instruction.getTargetRegister());
+			if (!rt.isPresent()) error("Target register not found.");
+
+			if (rt.get().getValue() < 0) return;
+
+			Register pc = set.getProgramCounter();
+
+			pc.setValue(pc.getValue() + (instruction.getImmediateAsSigned() << 2));
+		}
+	}
+}
