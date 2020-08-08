fc23f81ea1f36f01cecc2e87a44965bdcb981209
Gael Rial Costas <gael.rial.costas@gmail.com>
2020-08-08 14:52:57 +0200

    Added interrupt cause enum.

---
+++ src/main/java/net/jamsimulator/jams/mips/interrupt/InterruptCause.java
@@ -0,0 +1,51 @@
+package net.jamsimulator.jams.mips.interrupt;
+
+public enum InterruptCause {
+
+	INTERRUPT("Int", 0x00),
+	TLB_MODIFICATION_EXCEPTION("Mod", 0x01),
+	TLB_LOAD_EXCEPTION("TLBL", 0x02),
+	TLB_STORE_EXCEPTION("TLBS", 0x03),
+	ADDRESS_LOAD_EXCEPTION("AdEL", 0x04),
+	ADDRESS_STORE_EXCEPTION("AdES", 0x05),
+	INSTRUCTION_FETCH_BUS_EXCEPTION("IBE", 0x06),
+	DATA_BUS_EXCEPTION("DBE", 0x07),
+	SYSCALL_EXCEPTION("Sys", 0x08),
+	BREAKPOINT_EXCEPTION("Bp", 0x09),
+	RESERVED_INSTRUCTION_EXCEPTION("RI", 0x0A),
+	COPROCESSOR_UNUSABLE_EXCEPTION("CpU", 0x0B),
+	ARITHMETIC_OVERFLOW_EXCEPTION("Ov", 0x0C),
+	TRAP_EXCEPTION("Tr", 0x0D),
+	MSA_FLOATING_POINT_EXCEPTION("MSAFPE", 0x0E),
+	FLOATING_POINT_EXCEPTION("MSAFPE", 0x0F),
+	// 0x10, 0x11
+	COPROCESSOR_2_EXCEPTION("C2E", 0x12),
+	TLB_READ_INHIBIT_EXCEPTION("TLBRI", 0x13),
+	TLB_EXECUTION_INHIBIT_EXCEPTION("TLBXI", 0x14),
+	MSA_DISABLED_EXCEPTION("MSADis", 0x15),
+	MDMX_UNUSABLE_EXCEPTION("MDMX", 0X16),
+	REFERENCE_TO_WATCH("WATCH", 0X17),
+	MACHINE_CHECK("MCheck", 0x18),
+	THREAD_EXCEPTION("Thread", 0x19),
+	DPS_DISABLED_EXCEPTION("DSPDis", 0x1A),
+	VIRTUALIZED_GUEST_EXCEPTION("GE", 0x1B),
+	// 0x1C, 0x1D
+	CACHE_ERROR("CacheErr", 0x1E);
+	// 0x1F
+
+	private final String mnemonic;
+	private final int value;
+
+	InterruptCause(String mnemonic, int value) {
+		this.mnemonic = mnemonic;
+		this.value = value;
+	}
+
+	public String getMnemonic() {
+		return mnemonic;
+	}
+
+	public int getValue() {
+		return value;
+	}
+}
