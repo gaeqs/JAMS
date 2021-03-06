ce1901099ac728f1c00698450d97fc8a1f95a68a
gaeqs <gael.rial.costas@gmail.com>
2021-03-07 00:56:08 +0100

    Added addi pseudoinstruction.

---
+++ src/main/java/net/jamsimulator/jams/mips/instruction/pseudo/defaults/PseudoInstructionAddi.java
@@ -0,0 +1,60 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Gael Rial Costas
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.jamsimulator.jams.mips.instruction.pseudo.defaults;
+
+import net.jamsimulator.jams.mips.instruction.assembled.AssembledInstruction;
+import net.jamsimulator.jams.mips.instruction.basic.defaults.InstructionAdd;
+import net.jamsimulator.jams.mips.instruction.basic.defaults.InstructionAddiu;
+import net.jamsimulator.jams.mips.instruction.pseudo.PseudoInstruction;
+import net.jamsimulator.jams.mips.instruction.set.InstructionSet;
+import net.jamsimulator.jams.mips.parameter.InstructionParameterTypes;
+import net.jamsimulator.jams.mips.parameter.ParameterType;
+import net.jamsimulator.jams.mips.parameter.parse.ParameterParseResult;
+
+public class PseudoInstructionAddi extends PseudoInstruction {
+
+    public static final String MNEMONIC = "addi";
+
+    public static final InstructionParameterTypes PARAMETER_TYPES = new InstructionParameterTypes(ParameterType.REGISTER, ParameterType.REGISTER, ParameterType.SIGNED_16_BIT);
+
+    public PseudoInstructionAddi() {
+        super(MNEMONIC, PARAMETER_TYPES);
+    }
+
+    @Override
+    public int getInstructionAmount(String[] parameters) {
+        return 2;
+    }
+
+    @Override
+    public AssembledInstruction[] assemble(InstructionSet set, int address, ParameterParseResult[] parameters) {
+        var instructions = instructions(set, InstructionAddiu.class, InstructionAdd.class);
+
+        var addiu = parameters(AT, ZERO, parameters[2]);
+        var add = parameters(parameters[0], AT, parameters[1]);
+
+        return assemble(instructions, addiu, add);
+    }
+}
